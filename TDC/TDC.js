ae788f858776620963b110b534f839c72e4cba46
FacundoPiaggio <fpiaggio@frba.utn.edu.ar>
2025-01-12 22:03:13 -0300

    Feat: Actualizo TP

--- TDC/TDC.js
+++ TDC/TDC.js
@@ -1,10 +1,121 @@
+
+const ChartSalida = document.getElementById('ChartSalida').getContext('2d');
+const ChartMedicion = document.getElementById('ChartMedicion').getContext('2d');
+const ChartError = document.getElementById('ChartError').getContext('2d');
+
+const dataSalida = {
+    labels: [], // X-axis (time or iteration)
+    datasets: [{
+        label: 'Evolucion de la Salida',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        data: [],
+        fill: false,
+    }]
+};
+
+const configSalida = {
+    type: 'line',
+    data: dataSalida,
+    options: {
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom',
+                title: {
+                    display: true,
+                    text: 'Tiempo (segundos)'
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: 'Valor de Salida'
+                }
+            }
+        }
+    }
+};
+
+const dataMedicion = {
+    labels: [], // X-axis (time or iteration)
+    datasets: [{
+        label: 'Evolucion de la Medicion',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        data: [],
+        fill: false,
+    }]
+};
+
+const configMedicion = {
+    type: 'line',
+    data: dataMedicion,
+    options: {
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom',
+                title: {
+                    display: true,
+                    text: 'Tiempo (segundos)'
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: 'Valor de Medicion'
+                }
+            }
+        }
+    }
+};
+
+const dataError = {
+    labels: [], // X-axis (time or iteration)
+    datasets: [{
+        label: 'Evolucion del Error',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        data: [],
+        fill: false,
+    }]
+};
+
+const configError = {
+    type: 'line',
+    data: dataError,
+    options: {
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom',
+                title: {
+                    display: true,
+                    text: 'Tiempo (segundos)'
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: 'Valor de Error'
+                }
+            }
+        }
+    }
+};
+
+const salidaChart = new Chart(ChartSalida, configSalida);
+const medicionChart = new Chart(ChartMedicion, configMedicion);
+const errorChart = new Chart(ChartError, configError);
+
 const entrada = 2;
+var salida = 0;
+var tiempo = 0;
 
 const medicion = document.getElementById('slider');
 const valorMostrado = document.getElementById('sliderValue');
 let valorMedido = parseFloat(medicion.value);
-
-
 
 const kp = 2;
 const kd = 1;
@@ -30,75 +141,185 @@ medicion.addEventListener('input', () => {
 });
 
 const aplicarControlDerivativo = (variableAControlar) => {
+    
+    return entrada;
+    /*
+    var diferencia = variableAControlar - (entrada * kd);
+    diferencia = Math.abs(diferencia);
+    
+    return diferencia >= 10 ? diferencia - 10 : diferencia - entrada;*/
+
+    /*
     if(variableAControlar >= 3){
         return 0;
     }
     if(variableAControlar < 0){
         return 2;
     }
-    else return 0;
+    else return 0;*/
 }
 
 const aplicarControlProporcional = (variableAControlar) => {
+    
+    var diferencia;
+    if(variableAControlar > 2){
+        diferencia = entrada - variableAControlar;
+    } else if (variableAControlar < 2){
+        diferencia = variableAControlar - entrada;
+    } else {
+        return variableAControlar;
+    }
+    
+    var correccion;
+    if(variableAControlar > 2){
+        correccion = variableAControlar + diferencia;
+    } else{
+        console.log(`valor de diferencia: ${diferencia}`);
+        correccion = variableAControlar - diferencia;
+    }
+    return correccion;
+    
+    /*
     if(variableAControlar > 3){
         return variableControlada = variableAControlar >= 3 ? 3 : variableAControlar;
     }
-    
-    if(variableAControlar < -8){
-        return ((variableAControlar + 8)/(variableAControlar + 8))*kp;
+    */
+   /*
+    if(variableAControlar <= -8){
+        return 1.5;
     } 
     else if (variableAControlar < -6 && variableAControlar >= -8){
-        return ((variableAControlar + 8)/(variableAControlar + 8))*kp;
+        return 1.6;
     } 
-    else if (variableAControlar < -4 && variableAControlar >= -6){
-        return ((variableAControlar + 6)/(variableAControlar + 6))*kp;
+    else if (variableAControlar < -5 && variableAControlar >=-6){
+        return 1.7
     }
-    else if (variableAControlar < -2 && variableAControlar >= -4){
-        return ((variableAControlar + 4)/(variableAControlar + 4))*kp;
+    else if (variableAControlar < -4 && variableAControlar >= -5){
+        return 1.8;
     }
-    else if (variableAControlar < 0 && variableAControlar >= -2){
-        return ((variableAControlar + 2)/(variableAControlar + 2))*kp;
+    else if (variableAControlar <-3 && variableAControlar >= -4){
+        return 1.9;
     }
-    else if (variableAControlar < 3 && variableAControlar >= 1.5){
-        return variableAControlar;
+    else if (variableAControlar === -3){
+        return 2;
     }
-    else return 1.5;
+    else if (variableAControlar < -2 && variableAControlar > -3){
+        return 2.2;
+    }
+    else if (variableAControlar < -1 && variableAControlar >= -2){
+        return 2.4;
+    }
+    else if (variableAControlar < 0 && variableAControlar >= -1){
+        return 2.6;
+    }
+    else if (variableAControlar < 1 && variableAControlar >= -2){
+        return 2.8;
+    }
+    else return 3*/
+    /*else return 1.5;*/
 }
 
-console.log(aplicarControlDerivativo(5));
-console.log(aplicarControlDerivativo(-4));
-console.log(`control propo de 5: ${aplicarControlProporcional(5)}`);
-console.log(`control propo de 2.5: ${aplicarControlProporcional(2.5)}`);
-console.log(`control propo de 0: ${aplicarControlProporcional(0)}`);
-console.log(`control propo de -1: ${aplicarControlProporcional(-1)}`);
-console.log(`control propo de -3: ${aplicarControlProporcional(-3)}`);
-console.log(`control propo de -5: ${aplicarControlProporcional(-5)}`);
-console.log(`control propo de -7: ${aplicarControlProporcional(-7)}`);
-console.log(`control propo de -9: ${aplicarControlProporcional(-9)}`);
 
+const aplicarPerturbacion = (variableAControlar) => {
+    return variableAControlar + 10;
+
+}
 
 
 const aplicarControl = (variableAControlar) => {
+    
     var resultadoControlDerivativo = 0;
+    var resultadoControlProporcional = 0
+    console.log(`valor de variableAControlar: ${variableAControlar}`);
     if(perturbacion === "si"){
         resultadoControlDerivativo = aplicarControlDerivativo(variableAControlar);
+       // console.log(`valor de resultadoControlDerivativo: ${resultadoControlDerivativo}`);
+        perturbacion = "no";
+        document.getElementById("perturbacion").innerText = "Perturbacion: " + perturbacion;
+        return (aplicarControlProporcional(variableAControlar) + aplicarControlDerivativo(variableAControlar))/2;
+    } else {
+        return aplicarControlProporcional(variableAControlar);
     }
-    const resultadoControlProporcional = aplicarControlProporcional(variableAControlar);
+    resultadoControlProporcional = aplicarControlProporcional(variableAControlar);
+    
+    if(resultadoControlDerivativo != 0){
+        
+    }
+    
+    console.log(`valor de resultadoControlProporcional: ${resultadoControlProporcional}`);
 
-    const resultadoControl = resultadoControlDerivativo + resultadoControlProporcional;
-
-    return resultadoControl > 3 ? 3 : resultadoControl;
+    var resultadoControl = 0;
+    if (resultadoControlDerivativo != 0){
+        resultadoControl = resultadoControlDerivativo + resultadoControlProporcional;
+    } else {
+        resultadoControl = resultadoControlProporcional;
+    }
+    return resultadoControl;
+    //return resultadoControl > 3 ? 3 : resultadoControl;
 } 
 
-console.log(`_______________________ `);
-console.log(`control total de -4: ${aplicarControl(-4)}`);
-console.log(`control total de -5: ${aplicarControl(-5)}`);
-console.log(`control total de -6: ${aplicarControl(-6)}`);
-console.log(`control total de 5: ${aplicarControl(5)}`);
-console.log(`control total de 2: ${aplicarControl(2)}`);
+const calcularError = () => {
+    var e = 0;
+    
 
+    if (f === 0){
+        e = entrada - f;
+    } e
+}
 
 setInterval(() => {
+    
+    f = parseFloat(medicion.value);
+    if(perturbacion == "si"){
+        f = aplicarPerturbacion(f);
+    }
+    e = entrada - f
+    
+    console.log(`valor de f: ${f}`);
+    console.log(`valor de e: ${e}`);
+    
+    var salida = aplicarControl(e);
+    console.log(`valor de salida: ${salida}`);
+
+    const currentTime = dataSalida.labels.length;
+    dataSalida.labels.push(currentTime);
+    dataSalida.datasets[0].data.push(salida);
+
+    if (dataSalida.labels.length > 100) {
+        dataSalida.labels.shift();
+        dataSalida.datasets[0].data.shift();
+    }
+    salidaChart.update();
+
+    const currentTimeM = dataMedicion.labels.length;
+    dataMedicion.labels.push(currentTimeM);
+    dataMedicion.datasets[0].data.push(f);
+
+    if (dataMedicion.labels.length > 100) {
+        dataMedicion.labels.shift();
+        dataMedicion.datasets[0].data.shift();
+    }
+    medicionChart.update();
+
+    const currentTimeE = dataError.labels.length;
+    dataError.labels.push(currentTimeE);
+    dataError.datasets[0].data.push(e);
+
+    if (dataError.labels.length > 100) {
+        dataError.labels.shift();
+        dataError.datasets[0].data.shift();
+    }
+    errorChart.update();
+
+    /*
+    var variableControlada = aplicarControlProporcional(e);
+    console.log(`valor de f: ${f}`);
+    console.log(`valor de e: ${e}`);
+    console.log(`valor variableControlada: ${variableControlada}`);
+    */
+
+
+    /*
     f = parseFloat(medicion.value);
     e = entrada - f;
     var variableControlada = aplicarControl(e);
@@ -108,6 +329,6 @@ setInterval(() => {
 
     console.log(`valor de f: ${f}`);
     console.log(`valor de e: ${e}`);
-    console.log(`valor controlado: ${variableControlada}`)
+    console.log(`valor controlado: ${variableControlada}`)*/
     
-}, 5000);
+}, 1000);
